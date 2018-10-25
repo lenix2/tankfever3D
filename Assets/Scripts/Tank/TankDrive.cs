867b4d4873f193c7059b024fcdd6946fb3dc1a40
Lenny <lenny.r@live.ce>
2018-10-25 17:23:55 +0200

    beginning of tank movement

---
+++ Assets/Scripts/Tank/TankDrive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TankDrive : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (!isLocalPlayer)
+		{
+			Destroy(this);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Drive();
+	}
+
+	private void Drive()
+	{
+		this.gameObject.transform.position += this.gameObject.transform.forward * Time.deltaTime * 6f;
+	}
+}
